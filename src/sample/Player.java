b3640d84072788548ea03eee8d827555c7afab97
KNikovv <kaloyannikov@gmail.com>
2016-06-13 18:31:44 +0300

    added basic movement

--- src/sample/Player.java
+++ src/sample/Player.java
@@ -2,15 +2,14 @@ package sample;
 
 
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
 public class Player extends Pane {
-    Image survivorImg = new Image(getClass().getResourceAsStream("resources/playerSprite.png"));
-    ImageView imageView = new ImageView(survivorImg);
+    Image playerImg = new Image(getClass().getResourceAsStream("resources/playerSprite.png"));
+    ImageView imageView = new ImageView(playerImg);
     int count = 4;
     int columns = 4;
     int offsetX = 0;
@@ -18,20 +17,21 @@ public class Player extends Pane {
     int width = 40;
     int height = 40;
     public SpriteAnimation animation;
-
-    //char position on the "matrix"...
-    int posX;
-    int posY;
+    boolean isMovingRight ;
 
     public Player() {
-        imageView.setFitHeight(100);
-        imageView.setFitWidth(100);
+        imageView.setFitHeight(40);
+        imageView.setFitWidth(40);
         imageView.setViewport(new Rectangle2D(offsetX, offsetY, width, height));
-        animation = new SpriteAnimation(this.imageView, Duration.millis(500), count, columns, offsetX, offsetY, width, height);
+        animation = new SpriteAnimation(this.imageView, Duration.millis(500), count-1, columns, offsetX, offsetY, width, height);
         getChildren().addAll(this.imageView);
     }
 
-    public void move(int value) {
-
+    public void move() {
+        if(isMovingRight){
+            this.setTranslateX(this.getTranslateX()+1);
+        }else{
+            this.setTranslateX(this.getTranslateX()-1);
         }
     }
+}
--- src/sample/SinglePlayerState.java
+++ src/sample/SinglePlayerState.java
@@ -1,32 +1,66 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.HashMap;
+
 public class SinglePlayerState {
 
+    Image background = new Image(getClass().getResourceAsStream("resources/background.jpg"));
     Player player;
+    private HashMap<KeyCode, Boolean> keys = new HashMap<>();
+
     public SinglePlayerState() {
         this.createPlayScene();
+    }
+    private void update(){
+
+        if (isPressed(KeyCode.RIGHT)) {
+            player.animation.play();
+            player.isMovingRight=true;
+            player.move();
+
+        }
+        if (isPressed(KeyCode.LEFT)) {
+            player.animation.play();
+            player.isMovingRight=false;
+            player.move();
+        }
+    }
+    private boolean isPressed(KeyCode key) {
+        return keys.getOrDefault(key, false);
     }
 
     private void createPlayScene() {
         Pane pane = new Pane();
         pane.setPrefSize(900, 600);
-        Image image = new Image(getClass().getResourceAsStream("resources/background.jpg"));
         player = new Player();
         player.setTranslateX(400);
         player.setTranslateY(300);
-        ImageView imageView = new ImageView(image);
+        ImageView imageView = new ImageView(background);
         pane.getChildren().addAll(imageView,player);
         Stage stage = new Stage();
         Scene scene = new Scene(pane);
 
+        scene.setOnKeyPressed(event -> keys.put(event.getCode(), true));
+        scene.setOnKeyReleased(event -> {
+            keys.put(event.getCode(), false);
+            player.animation.stop();
+        });
         stage.setScene(scene);
-        stage.setAlwaysOnTop(true);
         stage.show();
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                update();
+            }
+        };
+        timer.start();
     }
 }
