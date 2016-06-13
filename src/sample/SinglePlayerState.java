6e9c57b468c16a5421966253a08fa66116479918
KNikovv <kaloyannikov@gmail.com>
2016-06-13 17:16:16 +0300

    add Background to playState

--- src/sample/SinglePlayerState.java
+++ src/sample/SinglePlayerState.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -12,9 +14,13 @@ public class SinglePlayerState {
 
     private void createPlayScene() {
         Pane pane = new Pane();
-        pane.setPrefSize(900, 600);
+        pane.setPrefSize(901, 601);
+        Image image = new Image(getClass().getResourceAsStream("resources/background.jpg"));
+        ImageView imageView = new ImageView(image);
+        pane.getChildren().add(imageView);
         Stage stage = new Stage();
         Scene scene = new Scene(pane);
+
         stage.setScene(scene);
         stage.setAlwaysOnTop(true);
         stage.show();
