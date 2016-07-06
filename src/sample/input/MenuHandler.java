98d4fb4b6627977e35c20fd7c84b54508c7fe24c
pepopln <petarmiroslavovnqgolov@abv.bg>
2016-07-06 21:45:14 +0300

    Add files via upload

--- src/sample/input/MenuHandler.java
+++ src/sample/input/MenuHandler.java
@@ -6,18 +6,20 @@ import javafx.stage.Stage;
 import sample.models.menumodels.Options;
 import sample.stages.CreditsStage;
 import sample.stages.HelpStage;
-import sample.stages.Highscore;
 import sample.stages.PlayState;
 
 public class MenuHandler {
 
     private Scene scene;
     private Options menu;
+    private boolean isMultiplayer;
 
     public MenuHandler(Scene scene, Options menuoptions) {
         this.scene = scene;
         this.menu = menuoptions;
+
     }
+
 
 
     public Scene getScene() {
@@ -27,8 +29,8 @@ public class MenuHandler {
 
     public void processMenuInput(Stage stage, Scene scene) {
 
-        this.menu.getItem(0).setOnMousePressed(e-> new PlayState(stage,scene).visualize());
-        this.menu.getItem(2).setOnMousePressed(e-> new Highscore(stage,scene).visualize());
+        this.menu.getItem(0).setOnMousePressed(e-> new PlayState(stage,scene,false).visualize());
+        this.menu.getItem(1).setOnMousePressed(event -> new PlayState(stage,scene,true).visualize());
         this.menu.getItem(3).setOnMousePressed(e-> new HelpStage(stage,scene).visualize());
         this.menu.getItem(4).setOnMousePressed(e-> new CreditsStage(stage,scene).visualize());
         this.menu.getItem(5).setOnMousePressed(e-> Platform.exit());
